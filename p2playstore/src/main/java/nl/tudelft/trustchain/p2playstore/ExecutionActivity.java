53a2febd5ea639abda1b9093aa8d970f370ad96d
Stan Verlaan <s.verlaan@student.tudelft.nl>
2025-05-21 11:17:37 +0200

    First attempt at running the search.apk

---
+++ p2playstore/src/main/java/nl/tudelft/trustchain/p2playstore/ExecutionActivity.java
@@ -0,0 +1,7 @@
+package nl.tudelft.trustchain.p2playstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ExecutionActivity extends AppCompatActivity {
+
+}
--- p2playstore/src/main/java/nl/tudelft/trustchain/p2playstore/P2PMainActivity.kt
+++ p2playstore/src/main/java/nl/tudelft/trustchain/p2playstore/P2PMainActivity.kt
@@ -1,19 +1,46 @@
 package nl.tudelft.trustchain.p2playstore
 
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
 import nl.tudelft.trustchain.common.BaseActivity
+import nl.tudelft.trustchain.p2playstore.databinding.ActivityMainBinding
+import nl.tudelft.trustchain.p2playstore.databinding.FragmentHomeBinding
 
-class P2PMainActivity() : BaseActivity() {
+const val DEFAULT_APK = "search.apk"
+
+class P2PMainActivity : BaseActivity() {
+    private lateinit var binding: FragmentHomeBinding
+
     override val navigationGraph = R.navigation.nav_graph_p2pstore
 
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
-//        setContentView(R.layout.activity_main)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
-//    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = FragmentHomeBinding.inflate(layoutInflater)
 
+        start()
+    }
+
+    private fun start() {
+        val continueButton = binding.continueButton
+        val fileName = DEFAULT_APK
+        continueButton.text = fileName
+
+        continueButton.setOnClickListener {
+            loadDynamicCode(fileName)
+        }
+    }
+
+    private fun loadDynamicCode(fileName: String) {
+        try {
+            val intent = Intent(this, ExecutionActivity::class.java)
+            intent.putExtra(
+                "fileName",
+                "${this.applicationContext.cacheDir}/${fileName.split("/").last()}"
+            )
+            startActivity(intent)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
 }
