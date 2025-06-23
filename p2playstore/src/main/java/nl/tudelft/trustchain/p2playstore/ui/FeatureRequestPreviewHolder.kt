8519436904198ff4cb80ce3c6e91bbfdcf077cc5
matthijsreyers <matthijscrafter@gmail.com>
2025-06-23 11:45:14 +0200

    Make button clickable instead of card

--- p2playstore/src/main/java/nl/tudelft/trustchain/p2playstore/ui/FeatureRequestPreviewHolder.kt
+++ p2playstore/src/main/java/nl/tudelft/trustchain/p2playstore/ui/FeatureRequestPreviewHolder.kt
@@ -10,7 +10,8 @@ import nl.tudelft.trustchain.p2playstore.models.FeatureRequest
 
 class FeatureRequestPreviewHolder(
     private val binding: FeatureRequestPreviewBinding,
-    private val dest: Int = R.id.action_featureListFragment_to_featureSolutionFragment
+    private val destBtn: Int = R.id.action_featureListFragment_to_featureSolutionFragment,
+    // private val dest: Int = R.id.action_featureListFragment_to_featureSolutionFragment,
 ) : RecyclerView.ViewHolder(binding.root) {
 
     fun hide() {
@@ -22,21 +23,41 @@ class FeatureRequestPreviewHolder(
         binding.title.text = request.title
         binding.description.text = request.description
         binding.reward.text = "Reward: ${request.reward} sats"
-        val solutions = request.getSolutions().size
-        if (solutions == 1) {
+
+        val solutions = request.getSolutions()
+        if (solutions.size == 1) {
             binding.solutionCount.text = "1 solution"
         } else {
-            binding.solutionCount.text = "${solutions} solutions"
+            binding.solutionCount.text = "${solutions.size} solutions"
         }
-        binding.card.setOnClickListener {
-            itemView.findNavController()
-                .navigate(
-                    dest,
-                    Bundle().apply {
-                        putString("daoId", request.doaId)
-                        putString("featureRequestId", request.featureRequestId)
-                    }
-                )
+
+        // binding.card.setOnClickListener {
+        //     itemView.findNavController()
+        //         .navigate(
+        //             dest,
+        //             Bundle().apply {
+        //                 putString("daoId", request.doaId)
+        //                 putString("featureRequestId", request.featureRequestId)
+        //             }
+        //         )
+        // }
+
+        val open = solutions.any { s -> !s.isPending } || solutions.isEmpty()
+        binding.submitBtn.isEnabled = open
+        binding.submitBtn.alpha = if (open) { 1.0f } else { 0.3f }
+        binding.submitBtn.setOnClickListener {
+            if (open) {
+                itemView.findNavController()
+                    .navigate(
+                        destBtn,
+                        Bundle().apply {
+                            putString("daoId", request.doaId)
+                            putString("featureRequestId", request.featureRequestId)
+                        }
+                    )
+            }
         }
+
+        binding.status.text = if (open) { "Open for solutions" } else { "Solution found" }
     }
 }
